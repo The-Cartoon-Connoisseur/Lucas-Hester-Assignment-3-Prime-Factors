42bafc5d95e917c1a908cf7032388b7a98c929e1
Lucas Hester <lucashester@icloud.com>
2025-02-21 13:23:23 -0500

    Test 2
    When generate_prime_factors is called with a 1, an empty list is returned

--- prime.py
+++ prime.py
@@ -1,3 +1,7 @@
 def generate_prime_factors(given_value):
     if not isinstance(given_value, int): #Checks to see if given_value is an integer
         raise ValueError() #If given_value is not an integer, raise an error
+
+    if given_value == 1: #If given_value is 1, return an empty list.
+        prime_factors_list = []
+        return prime_factors_list
--- test_generate_prime_factors.py
+++ test_generate_prime_factors.py
@@ -9,3 +9,8 @@ def test_not_integer_value_error():
         prime.generate_prime_factors(my_string)
     with pytest.raises(ValueError):
         prime.generate_prime_factors(my_float)
+
+#Test 2
+def test_prime_factors_empty_list_1():
+    my_test_empty_list = []
+    assert my_test_empty_list == prime.generate_prime_factors(1)
